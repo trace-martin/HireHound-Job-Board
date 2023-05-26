ce7715018e97edaec58077b09a9295e21e616b5d
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-05-25 20:34:04 -0600

    Created controllers file structure

---
+++ controllers/api/jobRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Job } = require('../../models');
+
+// GET All Jobs by User ID
+
+
+// GET Job by ID
+
+
+// POST Job
+
+
+// DELETE Job
+
+
+
+module.exports = router;
