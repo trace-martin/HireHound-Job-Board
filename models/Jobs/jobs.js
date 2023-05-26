83f0c091c35b570ad459acfd13d222c2dbb55664
Courtney Sherman <csherman177@gmail.com>
2023-05-25 19:35:14 -0600

    added the models

---
+++ models/Jobs/jobs.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Job extends Model {}
+
+Job.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  role_name: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  company_name: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  website: {
+    type: DataTypes.TEXT,
+  },
+  date_posted: {
+    type: DataTypes.DATE,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "user",
+      key: "id",
+    },
+  },
+}),
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Job",
+  };
+
+module.exports = Job;
