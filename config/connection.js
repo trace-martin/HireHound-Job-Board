9337254d0a4cea9c9988b1fae5fa34644b7d6ee5
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-05-30 19:05:34 -0600

    Added server.js code and homepage route

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
-// if (process.env.???) {
-//   sequelize = new Sequelize(process.env.????);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// }
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST ? process.env.DB_HOST : "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,2 +1,54 @@
-const express = require('express');
-const routes = require('./controllers');
+// Import modules
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// Import objects/instances
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+
+// Create express server app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Create a session object and link it with sequelize
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    //maxAge: 1800000  // Session expires after 30 minutes
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Set handlebars engine settings
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Express server middleware required
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Express server routes
+app.use(routes);
+
+// Sync sequelize models and run server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      `App listening, launch app in browser: http://localhost:${PORT}`
+    );
+  });
+});
