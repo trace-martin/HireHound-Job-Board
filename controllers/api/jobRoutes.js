e54d42a41122b9986c6aba8c26aea6e72814394d
Courtney Sherman <csherman177@gmail.com>
2023-05-30 19:12:22 -0600

    added the routes

--- controllers/api/jobRoutes.js
+++ controllers/api/jobRoutes.js
@@ -1,17 +1,66 @@
-const router = require('express').Router();
-const { Job } = require('../../models');
+const router = require("express").Router();
+const { Job, User } = require("../../models");
 
 // GET All Jobs by User ID
-
+router.get("/", async (req, res) => {
+  try {
+    const allJobs = await Job.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    res.json(allJobs);
+  } catch (error) {
+    res.json(err);
+  }
+});
 
 // GET Job by ID
-
+router.get("/:id", async (req, res) => {
+  try {
+    const allJobs = await Job.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    res.json(allJobs);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 // POST Job
-
+router.get("/:id", async (req, res) => {
+  try {
+    const postJobs = await Job.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    res.json(postJobs);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 // DELETE Job
 
-
+router.delete("/:id", (req, res) => {
+  Job.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deleteJob) => {
+      res.json(deleteJob);
+    })
+    .catch((err) => res.json(err));
+});
 
 module.exports = router;
